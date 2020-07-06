c3dacbb16376a55bffee498f558eaa885c28e473
manan2812 <64685274+manan2812@users.noreply.github.com>
2020-07-06 22:25:34 +0530

    Add files via upload

---
+++ checking.py
@@ -0,0 +1,10 @@
+programfile = open('/root/Desktop/ml-automation/machinelearning_code.py','r')	#connecting to the code file
+code = programfile.read()				#reading the code file
+
+if 'keras' or 'tensorflow' in code:			#because keras or tensorflow keyword is a must for a deep learning program
+	if 'Conv2D' in code:				#beacuse if a code is of CNN conv2D layer is a must 
+		print('OK')
+	else:
+		print('not OK')
+else:
+	print('NOT A NEURAL NETWORK')
---
+++ model_less_mail.py
@@ -0,0 +1,15 @@
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "my@gmail.com"  # Enter your address
+receiver_email = "your@gmail.com"  # Enter receiver address
+password = input("Type your password and press enter: ")
+message = """\
+Subject: "Regarding failure of machinelearning code having less accuracy then the desired one"
+Model does not reach to desired accuracy."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
